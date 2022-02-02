50800bfe1ba2a5a83f2126c9b305f7e7ff6d9859
devtucuju <jsdeveloperweb@gmail.com>
2022-02-02 01:55:03 -0300

    adiciona loading de carregamento de localização

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,12 +2,17 @@ import React, {useState, useEffect} from 'react';
 import LocationCurrent from '../../services/locationCurrent';
 import MapView from 'react-native-maps';
 
-import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
-import {Snackbar} from 'react-native-paper';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Animated,
+} from 'react-native';
 import {useSelector} from 'react-redux';
 
 function Home() {
-  const status = useSelector(state => state.auth.status);
+  // const status = useSelector(state => state.auth.status);
   const [initialPosition, setInitialPosition] = useState({
     latitude: -23.7,
     longitude: -46.8,
@@ -16,19 +21,18 @@ function Home() {
   });
   const [loading, setLoading] = useState(false);
   const [loadingMsg, setLoadingMsg] = useState('');
-  console.log(initialPosition);
+  const [warnHeight, setwarnHeight] = useState(new Animated.Value(0));
+
   useEffect(() => {
+    setWarning(true, 'Procurando sua localização!');
     checkUserCoords();
   }, []);
 
   const checkUserCoords = async () => {
-    setLoading(true);
-    setLoadingMsg('Procurando sua localização!');
     let coord = await LocationCurrent().getLocation();
 
     if (coord && coord.latitude && coord.longitude) {
-      setLoading(false);
-      setLoadingMsg('');
+      setWarning(false, '');
       setInitialPosition({
         latitude: coord.latitude,
         longitude: coord.longitude,
@@ -36,16 +40,39 @@ function Home() {
         longitudeDelta: 0.0421,
       });
     } else {
-      setLoading(false);
-      setLoadingMsg('');
+      setWarning(false, '');
       setInitialPosition(null);
     }
   };
+
+  function setWarning(status, msg) {
+    if (status === true && msg !== '') {
+      setLoading(status);
+      setLoadingMsg(msg);
+      Animated.timing(warnHeight, {
+        toValue: 40,
+        duration: 1000,
+        useNativeDriver: false,
+      }).start();
+    } else if (!status) {
+      setLoading(status);
+      setLoadingMsg('');
+      Animated.timing(warnHeight, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: false,
+      }).start();
+    }
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle={'dark-content'} />
+      <StatusBar barStyle={'light-content'} />
 
       <MapView style={styles.map} region={initialPosition} />
+
+      <Animated.View style={[styles.warnbox, {height: warnHeight}]}>
+        <Text style={styles.text}>{loadingMsg}</Text>
+      </Animated.View>
     </SafeAreaView>
   );
 }
@@ -60,5 +87,18 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  warnbox: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    backgroundColor: '#000000',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 13,
+    color: '#fff',
   },
 });
